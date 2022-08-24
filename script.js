e1450694589926f9fa7745d94d4af233612922af
AmnaMuhammadAshraf <111840265+AmnaMuhammadAshraf@users.noreply.github.com>
2022-08-24 23:27:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+var swiper = new Swiper(".books-slider", {
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+  var swiper = new Swiper(".reviews-slider", {
+    spaceBetween: 10,
+    grabCursor:true,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+  var swiper= new Swiper(".featured-slider",{
+    loop:true,
+    spaceBetween: 10,
+    centeredSlides:true,
+    autoplay:{
+      delay:7500,
+      disableOnInteraction: false,
+
+
+
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints:{
+      0: {
+        slidesPerView: 1,
+      },
+      756:{
+        slidesPerView:2,
+      },
+      1024:{
+        slidesPerView:3,
+
+      },
+    },}
+  
+  );
